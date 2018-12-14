10fc4a836c321af845095935f107bb7b3d22b8dd
jscottsiri <jss82@njit.edu>
2018-12-14 15:59:11 -0500

    Added Isometric View

---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour {
+    int ScrollSpeed = 5;
+    public Transform target;
+	// Use this for initialization
+	void Start () {
+
+    }
+    
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButton(2))
+        {
+            if (Input.mousePosition.x >= Screen.width * 0.95)
+            {
+                transform.LookAt(target);
+                transform.Translate(Vector3.right * Time.deltaTime * ScrollSpeed);
+            }
+            else if (Input.mousePosition.x <= Screen.width * 0.05)
+            {
+                transform.LookAt(target);
+                transform.Translate(Vector3.left * Time.deltaTime * ScrollSpeed);
+            }
+            if (Input.mousePosition.y <= Screen.height * 0.05)
+            {
+                transform.LookAt(target);
+                transform.Translate(Vector3.down * Time.deltaTime * ScrollSpeed);
+            }
+            else if (Input.mousePosition.y >= Screen.height * 0.95)
+            {
+                transform.LookAt(target);
+                transform.Translate(Vector3.up * Time.deltaTime * ScrollSpeed);
+            }
+        }
+    }
+
+}
---
+++ Assets/Scripts/Transporter.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Transporter : MonoBehaviour {
+
+    public Transform Exit1;
+    public Transform Enter1;
+    public Transform Exit2;
+    public Transform Enter2;
+    public Transform chara;
+    public int fac;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+
+	void FixedUpdate () {
+        teleport();
+    }
+    void teleport ()
+    {
+        if (chara.position == Exit1.position)
+        {
+            chara.position = Enter2.position;
+        }
+        else if (chara.position == Exit2.position)
+        {
+            chara.position = Enter1.position;
+        }
+    }
+}
