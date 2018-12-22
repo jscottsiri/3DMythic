ce01e2c4038fa522fa9b0c0d2b5677fec2793b5c
jscottsiri <jscottsiri@gmail.com>
2018-12-21 23:31:03 -0500

    UI Stuff

---
+++ Assets/Scripts/HealthBar.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    public Text Hitpoints;
+    int hp;
+    public GameObject character;
+    private void Update()
+    {
+        hp = character.GetComponent<CharStats>().HP;
+        Hitpoints.text = "" + hp + "";
+    }
+    
+}
